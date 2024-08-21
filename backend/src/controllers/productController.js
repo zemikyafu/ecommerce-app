96dbd074f1b8b982b1486ca34513f084245f000b
zemikyafu <zemikfcb@gmail.com>
2024-08-21 16:33:17 +0300

    product service and controller created

---
+++ backend/src/controllers/productController.js
@@ -0,0 +1,63 @@
+const { Model } = require('sequelize');
+const productService=require('../services/productService')
+const createProduct=async (request,response)=>{
+    const product=request.body;
+    try {
+        const result= await productService.createProduct(product);
+        response.status(201).json(result);
+    } catch (error) {
+        console.error('Error while creating product:', error.message, error.stack);
+        response.status(500).send({ 'Error': 'Internal server error' });
+    }
+}
+
+const findAllProducts=async(request,response)=>{
+    try {
+        const users=await productService.findAllProducts();
+        response.status(200).json(users);
+    } catch (error) {
+        console.error('Error while fetching products:', error.message, error.stack);
+        response.status(500).send({ 'Error': 'Internal server error' });
+    }
+}
+ 
+const findProductById=async(request,response)=>{
+    try {
+        const id =request.params.id
+        if(!id)
+            return response.status(401).json({'error':'Missing request paramarer '})
+        const product=await productService.findProductById(id);
+       
+        if(product){
+            response.status(200).json(product);
+        }
+       else{
+        response.status(404).json({'error':'Product not found'});
+       }
+    } catch (error) {
+        console.error('Error while fetching products:', error.message, error.stack);
+        response.status(500).send({ 'Error': 'Internal server error' });
+    }
+}
+ 
+const updateProduct=async(request,response)=>{
+    try {
+        const id =request.params.id
+        const product=request.body
+        const result=await productService.updateProduct(id,product);
+        if(result){
+            response.status(200).json(result);
+        }
+       else{
+        response.status(404).json({'error':'Product not found'});
+       }
+    } catch (error) {
+        console.error('Error while fetching products:', error.message, error.stack);
+        response.status(500).send({ 'Error': 'Internal server error' });
+    }
+}
+
+module.exports.createProduct=createProduct
+module.exports.findAllProducts=findAllProducts
+module.exports.updateProduct=updateProduct
+module.exports.findProductById=findProductById
---
+++ backend/src/services/productService.js
@@ -0,0 +1,45 @@
+const Product=require('../models/products');
+
+const findAllProducts= async ()=>{
+    try {
+        const products= await Product.findAll();
+        return products
+    } catch (error) {
+        console.log('error',error);
+        return null
+    }
+}
+const findProductById=async (id)=>{
+    try {
+        const products=await Product.findAll({where:{id:id}});
+        return products;
+    } catch (error) {
+        console.log('error',error);
+        return null;
+    }
+}
+const createProduct=async (product)=>{
+    try {
+        const result=await Product.create(product);
+        return result;
+    } catch (error) {
+        console.log('error',error);
+        return null;
+    }
+}
+
+const updateProduct=(id,product)=>{
+       try {
+            const {name,description,price,image_url}=product;
+            const result=Product.update({name:name,description:description,price:price,image_url:image_url},{where:{id:id},returning:true});
+            return result;
+        } catch (error) {
+            console.log('error',error);
+            return null;
+        }
+}
+
+module.exports.createProduct=createProduct;
+module.exports.findAllProducts=findAllProducts;
+module.exports.findProductById=findProductById;
+module.exports.updateProduct=updateProduct;
